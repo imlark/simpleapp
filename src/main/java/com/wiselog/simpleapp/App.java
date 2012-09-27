d5eaa9521037892c8ecb8334954eeda3e48401bb
imlark <tolark@gmail.com>
2012-09-27 16:28:12 +0900

    second commit

---
+++ src/main/java/com/wiselog/simpleapp/App.java
@@ -0,0 +1,13 @@
+package com.wiselog.simpleapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
